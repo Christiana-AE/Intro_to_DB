2e7d3d354b52e94787782d04b2a633d559d32fe5
Christiana <christine.anako@gmail.com>
2025-08-22 19:31:55 +0200

    database

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,7 @@
 -- Select the target database
 USE alx_book_store;
 
+INSERT INTO Customer
 -- Insert a single row into the Customers table
-INSERT INTO Customer (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
